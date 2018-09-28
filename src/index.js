775ea265e568744ae1c6e3234282b98d330254ce
Olli Pajuluoma <olli.pajuluoma@leadin.fi>
2018-09-28 11:57:22 +0300

    Old basic structure (some modifications at utils transformer). Moving to new structure from Helmet.

--- src/index.js
+++ src/index.js
@@ -33,23 +33,32 @@ import MarcRecord from 'marc-record-js';
 import validateFactory from '@natlibfi/marc-record-validators-melinda';
 import {TransformerUtils as utils} from '@natlibfi/melinda-record-import-commons';
 import config from './config';
+//Filter dummys for testing filtering
+// //Development XML
+import {dummyXML} from './dummyXML.js';
+import {dummyJSON} from './dummyJSON.js';
 
 start();
 
 async function start() {
 	let validate;
-	const logger = utils.createLogger();
+	const logger = utils.createLogger(); //Shared
 
-	utils.registerSignalHandlers();
-	utils.checkEnv();
+	utils.registerSignalHandlers(); //Shared
+	utils.checkEnv([]); //Custom
+	console.log("Done");
 
-	const stopHealthCheckService = utils.startHealthCheckService(process.env.HEALTH_CHECK_PORT);
+	const stopHealthCheckService = utils.startHealthCheckService(process.env.HEALTH_CHECK_PORT); //Shared
+	console.log("1");
 
 	try {
-		validate = validateFactory(config.validators);
+		//validate = validateFactory(config.validators);
 
-		await utils.startTransformation(transform);
+		await utils.startTransformation(transform); //Custom //Modified to provide dummy response. 
+		//Starts function provided as callback ('transform') after fetching http response 
+		console.log("4");
 		stopHealthCheckService();
+		console.log("5");
 		process.exit();
 	} catch (err) {
 		stopHealthCheckService();
@@ -57,7 +66,58 @@ async function start() {
 		process.exit(-1);
 	}
 
+	//This is called as callback from startTransformation providing 
+	//Precondition:
+	//let response = await fetch(`${process.env.API_URL}/blobs/${process.env.BLOB_ID}/content`
+	//Postcondition:
+	//const records = await transformCallback(response);
 	async function transform(response) {
-		/** TODO **/
+		//response = dummyXML;
+		//console.log("Dummy: ", dummyXML);
+		console.log("Response: ", response); //Response from modified transformer is dummy response with mock-http
+		console.log("-----------------------");
+		console.log("JSON 1: ", response.json())
+		// const records = await response.json();
+		const records = dummyJSON;
+		console.log("JSON 2: ", records);
+
+		const convertedRecords = await Promise.all(records.map(convertRecord));
+		const validationResults = await Promise.all(convertedRecords.map(r => validate(r, {
+			fix: true,
+			validateFixes: true
+		})));
+
+		return convertedRecords.reduce((acc, record, index) => {
+			return acc.concat({
+				record,
+				failed: validationResults[index].failed,
+				messages: validationResults[index].validators
+			});
+		}, []);
+
+		function convertRecord(record) {
+			const marcRecord = new MarcRecord();
+			record.varFields.forEach(field => {
+				if (field.content) {
+					if (field.fieldTag === '_') {
+						marcRecord.setLeader(field.content);
+					} else {
+						marcRecord.insertControlField({tag: field.marcTag, value: field.content});
+					}
+				} else {
+					marcRecord.insertField({
+						tag: field.marcTag,
+						ind1: field.ind1,
+						ind2: field.ind2,
+						subfields: field.subfields.map(subfield => ({
+							code: subfield.tag,
+							value: subfield.content
+						}))
+					});
+				}
+			});
+
+			return marcRecord;
+		}
 	}
 }
