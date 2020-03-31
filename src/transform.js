f37ddd89ea58eb15ee27cc6edcd05ec12e984940
Olli Pajuluoma <36146641+OlliPaj@users.noreply.github.com>
2020-03-31 14:29:16 +0300

    Updated copyright years (#7)
    * Updated copyright years
    
    * Updated project descriptions
    
    * Merged dev changes

--- src/transform.js
+++ src/transform.js
@@ -2,9 +2,9 @@
 *
 * @licstart  The following is the entire license notice for the JavaScript code in this file.
 *
-* Helmet record transformer for the Melinda record batch import system
+* Publication archives record transformer for the Melinda record batch import system
 *
-* Copyright (C) 2019 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2019-2020 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of melinda-record-import-transformer-publication-archives
 *
