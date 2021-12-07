54bac4d34e2592e1e5e4da3287961f0f6ed2b4f0
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2021-12-07 10:36:52 +0200

    Clean tests

--- src/validate.spec.js
+++ src/validate.spec.js
@@ -32,24 +32,22 @@ import {READERS} from '@natlibfi/fixura';
 import generateTests from '@natlibfi/fixugen';
 import createValidator from './validate';
 
-describe('validate', () => {
-  generateTests({
-    path: [__dirname, '..', 'test-fixtures', 'validate'],
-    useMetadataFile: true,
-    recurse: false,
-    fixura: {reader: READERS.JSON, failWhenNotFound: false},
-    callback
-  });
-
-  async function callback({getFixture, isLegalDeposit}) {
-    const record = new MarcRecord(getFixture('input.json'));
-    const expectedResult = getFixture('output.json');
-
-    const validate = await createValidator(isLegalDeposit);
-    const result = await validate(record, {fix: true, validateFixes: true});
-
-    expect(result.record).to.eql(expectedResult.record);
-    expect(result.messages).to.eql(expectedResult.messages);
-    expect(result.fail).to.eql(expectedResult.fail);
-  }
+generateTests({
+  path: [__dirname, '..', 'test-fixtures', 'validate'],
+  useMetadataFile: true,
+  recurse: false,
+  fixura: {reader: READERS.JSON, failWhenNotFound: false},
+  callback
 });
+
+async function callback({getFixture, isLegalDeposit}) {
+  const record = new MarcRecord(getFixture('input.json'));
+  const expectedResult = getFixture('output.json');
+
+  const validate = await createValidator(isLegalDeposit);
+  const result = await validate(record, {fix: true, validateFixes: true});
+
+  expect(result.record).to.eql(expectedResult.record);
+  expect(result.messages).to.eql(expectedResult.messages);
+  expect(result.fail).to.eql(expectedResult.fail);
+}
