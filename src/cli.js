e2cc95bf8e602d249a94358dc26803c8fb66b948
aatuny <27770287+aatuny@users.noreply.github.com>
2024-08-16 13:34:30 +0300

    cli and config initial fixes

--- src/cli.js
+++ src/cli.js
@@ -9,7 +9,7 @@ cli();
 async function cli() {
   const args = yargs(process.argv.slice(2))
     .scriptName('melinda-record-import-transformer-publication-archives')
-    .epilog('Copyright (C) 2019-2022 University Of Helsinki (The National Library Of Finland)')
+    .epilog('Copyright (C) 2019-2024 University Of Helsinki (The National Library Of Finland)')
     .usage('$0 <file> [options] and env variable info in README')
     .showHelpOnFail(true)
     .example([
--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ import {readEnvironmentVariable} from '@natlibfi/melinda-backend-commons';
 
 export const profileIds = readEnvironmentVariable('PROFILE_IDS', {defaultValue: ['foobar'], format: v => JSON.parse(v)});
 export const amqpUrl = readEnvironmentVariable('AMQP_URL', {defaultValue: 'amqp://127.0.0.1:5672/'});
+export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1/db'});
 export const abortOnInvalidRecords = readEnvironmentVariable('ABORT_ON_INVALID_RECORDS', {defaultValue: false, format: v => parseBoolean(v)});
 
 export const harvestSource = readEnvironmentVariable('HARVEST_SOURCE');
@@ -18,7 +19,12 @@ export const filters = {
 
 export const recordImportApiOptions = {
   recordImportApiUrl: readEnvironmentVariable('RECORD_IMPORT_API_URL', {defaultValue: 'cli'}),
-  recordImportApiUsername: readEnvironmentVariable('RECORD_IMPORT_API_USERNAME_TRANSFORMER', {defaultValue: 'cli'}),
-  recordImportApiPassword: readEnvironmentVariable('RECORD_IMPORT_API_PASSWORD_TRANSFORMER', {defaultValue: 'cli', hideDefault: true}),
-  userAgent: readEnvironmentVariable('API_CLIENT_USER_AGENT', {defaultValue: '_RECORD-IMPORT-TRANSFORMER'})
+  userAgent: readEnvironmentVariable('API_CLIENT_USER_AGENT', {defaultValue: '_RECORD-IMPORT-TRANSFORMER'}),
+  allowSelfSignedApiCert: readEnvironmentVariable('ALLOW_API_SELF_SIGNED', {defaultValue: false, format: parseBoolean})
+};
+
+export const keycloakOptions = {
+  issuerBaseURL: readEnvironmentVariable('KEYCLOAK_ISSUER_BASE_URL', {defaultValue: 'KEYCLOAK_ISSUER_BASE_URL env is not set!'}),
+  serviceClientID: readEnvironmentVariable('KEYCLOAK_SERVICE_CLIENT_ID', {defaultValue: 'KEYCLOAK_SERVICE_CLIENT_ID env is not set!'}),
+  serviceClientSecret: readEnvironmentVariable('KEYCLOAK_SERVICE_CLIENT_SECRET', {defaultValue: 'KEYCLOAK_SERVICE_CLIENT_SECRET env is not set!'})
 };
