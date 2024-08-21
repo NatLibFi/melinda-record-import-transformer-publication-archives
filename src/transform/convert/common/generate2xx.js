0543a6cb92cf9df94b11a1c3fe86227f62da4988
aatuny <27770287+aatuny@users.noreply.github.com>
2024-08-21 16:12:23 +0300

    Add initial tests for 2xx generators

--- src/transform/convert/common/generate2xx.js
+++ src/transform/convert/common/generate2xx.js
@@ -5,20 +5,23 @@
  */
 export function generate245({getFields, getFieldValues}) {
   const isAddedEntry = generateIsAddedEntry();
-  const titles = getFieldValues('dc.title');
+  const [title] = getFieldValues('dc.title');
 
-  return titles.map(title => ({
-    tag: '245',
-    ind1: isAddedEntry ? '1' : '0',
-    ind2: '0',
-    subfields: [{code: 'a', value: `${title}.`}]
-  }));
+  return title ? [
+    {
+      tag: '245',
+      ind1: isAddedEntry ? '1' : '0',
+      ind2: '0',
+      subfields: [{code: 'a', value: `${title}.`}]
+    }
+  ] : [];
 
   function generateIsAddedEntry() {
     const fields = getFields(p => [
       'dc.contributor.author',
-      'dc.author'
+      'dc.creator'
     ].includes(p));
+
     return fields.length > 0;
   }
 }
@@ -59,10 +62,15 @@ export function generate264({getFieldValues}) {
   const subfields = generateSubfields();
 
   if (subfields.length > 0) {
-    return {
-      tag: '264', ind1: '', ind2: '1', subfields
-    };
+    return [
+      {
+        tag: '264', ind1: '', ind2: '1', subfields
+      }
+    ];
   }
+
+  return [];
+
 
   function generateSubfields() {
     const place = generatePlace();
@@ -83,11 +91,7 @@ export function generate264({getFieldValues}) {
 
     function generateIssueDate() {
       const values = getFieldValues('dc.date.issued');
-      return values.length > 0 ? values.map(toSubfield) : [];
-
-      function toSubfield(value) {
-        return {code: 'c', value: `${value}.`};
-      }
+      return values.length > 0 ? [{code: 'c', value: `${values[0]}.`}] : [];
     }
   }
 }
---
+++ src/transform/convert/common/generate2xx.spec.js
@@ -0,0 +1,106 @@
+import {expect} from 'chai';
+
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+
+import * as fieldGenerator from './generate2xx';
+import {createValueInterface} from '../util';
+
+// Run tests
+generate245();
+generate246();
+generate250();
+generate264();
+
+// Test functions
+function generate245() {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', '..', '..', 'test-fixtures', 'transform', 'convert', 'common', 'generate2xx', 'generate245'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      reader: READERS.JSON,
+      failWhenNotFound: true
+    }
+  });
+
+  function callback({getFixture}) {
+    const input = getFixture('input.json');
+    const output = getFixture('output.json');
+
+    const valueInteface = createValueInterface(input);
+
+    const result = fieldGenerator.generate245(valueInteface);
+    expect(result).to.eql(output);
+  }
+}
+
+function generate246() {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', '..', '..', 'test-fixtures', 'transform', 'convert', 'common', 'generate2xx', 'generate246'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      reader: READERS.JSON,
+      failWhenNotFound: true
+    }
+  });
+
+  function callback({getFixture}) {
+    const input = getFixture('input.json');
+    const output = getFixture('output.json');
+
+    const valueInteface = createValueInterface(input);
+
+    const result = fieldGenerator.generate246(valueInteface);
+    expect(result).to.eql(output);
+  }
+}
+
+function generate250() {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', '..', '..', 'test-fixtures', 'transform', 'convert', 'common', 'generate2xx', 'generate250'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      reader: READERS.JSON,
+      failWhenNotFound: true
+    }
+  });
+
+  function callback({getFixture}) {
+    const input = getFixture('input.json');
+    const output = getFixture('output.json');
+
+    const valueInteface = createValueInterface(input);
+
+    const result = fieldGenerator.generate250(valueInteface);
+    expect(result).to.eql(output);
+  }
+}
+
+function generate264() {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', '..', '..', 'test-fixtures', 'transform', 'convert', 'common', 'generate2xx', 'generate264'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      reader: READERS.JSON,
+      failWhenNotFound: true
+    }
+  });
+
+  function callback({getFixture}) {
+    const input = getFixture('input.json');
+    const output = getFixture('output.json');
+
+    const valueInteface = createValueInterface(input);
+
+    const result = fieldGenerator.generate264(valueInteface);
+    expect(result).to.eql(output);
+  }
+}
