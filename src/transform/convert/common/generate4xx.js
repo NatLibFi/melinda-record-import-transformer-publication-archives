178e91f3a28c844a46ab45c0ed5aa686acf78ada
aatuny <27770287+aatuny@users.noreply.github.com>
2024-08-22 11:12:13 +0300

    Add tests for field 4xx generators

--- src/transform/convert/common/generate4xx.js
+++ src/transform/convert/common/generate4xx.js
@@ -1,76 +1,83 @@
 /**
- * Generates field 490 ($a, $v, $x) if any of the subfields can be generated.
+ * Generates field 490 ($a, $v, $x) if subfields $a or $x can be generated.
  * Values creation is based on are found from dc.relation.ispartofseries,
  * dc.relation.numberofseries, dc.relation.numberinseries and dc.relation.issn.
  * @param {Object} ValueInterface containing getFieldValues function
- * @returns Undefined or object containing field 490 ($a, $v, $x)
+ * @returns Empty array or array containing field 490 ($a, $v, $x)
  */
 export function generate490({getFieldValues}) {
   const subfields = generateSubfields();
 
   if (subfields.length > 0) {
-    return {
-      tag: '490',
-      ind1: '0',
-      ind2: ' ',
-      subfields
-    };
+    return [
+      {
+        tag: '490',
+        ind1: '0',
+        ind2: ' ',
+        subfields
+      }
+    ];
   }
 
-  function generateSubfields() {
-    const tail = generateTail();
-    return generateIsPartOfSeries(tail.length > 0).concat(tail);
+  return [];
 
-    function generateIsPartOfSeries(hasTail) {
+
+  function generateSubfields() {
+    const subfieldV = generateSubfieldV();
+    const subfieldX = generateSubfieldX(subfieldV.length > 0);
+    const subfieldsXandV = [...subfieldX, ...subfieldV]; // NB: order is important ($x before $v)
+
+    const subfieldA = generateSubfieldA(subfieldsXandV.length > 0);
+
+    // Do not generate field if only subfield $v would be available
+    const fieldShoudlBeGenerated = subfieldA.length > 0 || subfieldX.length > 0;
+    return fieldShoudlBeGenerated ? subfieldA.concat(subfieldsXandV) : [];
+
+    function generateSubfieldV() {
+      return getFieldValues(p => p === 'dc.relation.numberinseries' || p === 'dc.relation.numberofseries')
+        .reduceRight((acc, value) => {
+          if (acc.length === 0) {
+            return acc.concat({code: 'v', value});
+          }
+
+          return acc.concat({code: 'v', value: `${value} ; `});
+        }, [])
+      // Reverse so that subfields with ';' separator are placed first
+        .reduceRight((acc, v) => acc.concat(v), []);
+    }
+
+    function generateSubfieldX(hasSeriesNumber) {
+      return getFieldValues('dc.relation.issn')
+        .reduceRight((acc, value) => {
+          if (acc.length === 0) {
+            return acc.concat({code: 'x', value: hasSeriesNumber ? `${value} ;` : value});
+          }
+
+          return acc.concat({code: 'x', value: `${value}, `});
+        }, [])
+      // Reverse so that subfields with ',' separator are placed first
+        .reduceRight((acc, v) => acc.concat(v), []);
+    }
+
+    function generateSubfieldA(hasOtherSubfields) {
       const values = getFieldValues('dc.relation.ispartofseries');
       return values
-        .reduceRight(toSubfields, [])
-        // Reverse
+        .reduceRight(toSubfieldA, [])
+        // Reverse so that subfields with ',' separator are placed first
         .reduceRight((acc, v) => acc.concat(v), []);
 
-      function toSubfields(acc, value) {
+      function toSubfieldA(acc, value) {
         const subfieldValue = generateValue();
         return acc.concat({code: 'a', value: subfieldValue});
 
         function generateValue() {
+          // NB: separator depends whether there are one or many subfield $a
           if (acc.length === 0) {
-            return hasTail ? `${value},` : value;
+            return hasOtherSubfields ? `${value},` : value;
           }
 
           return `${value} `;
         }
-      }
-    }
-
-    function generateTail() {
-      const seriesNumber = generateSeriesNumber();
-
-      return generateIssn(seriesNumber.length > 0).concat(seriesNumber);
-
-      function generateSeriesNumber() {
-        return getFieldValues(p => p === 'dc.relation.numberinseries' || p === 'dc.relation.numberofseries')
-          .reduceRight((acc, value) => {
-            if (acc.length === 0) {
-              return acc.concat({code: 'v', value});
-            }
-
-            return acc.concat({code: 'v', value: `${value} ; `});
-          }, [])
-          // Reverse
-          .reduceRight((acc, v) => acc.concat(v), []);
-      }
-
-      function generateIssn(hasSeriesNumber) {
-        return getFieldValues('dc.relation.issn')
-          .reduceRight((acc, value) => {
-            if (acc.length === 0) {
-              return acc.concat({code: 'x', value: hasSeriesNumber ? `${value} ;` : value});
-            }
-
-            return acc.concat({code: 'x', value: `${value}, `});
-          }, [])
-          // Reverse
-          .reduceRight((acc, v) => acc.concat(v), []);
       }
     }
   }
