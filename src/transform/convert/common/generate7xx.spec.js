b443cb72288e30d8638bd99180296ffb9dcfb7ea
aatuny <27770287+aatuny@users.noreply.github.com>
2024-08-22 15:18:15 +0300

    Add tests for field 7xx generators

---
+++ src/transform/convert/common/generate7xx.spec.js
@@ -0,0 +1,34 @@
+import {expect} from 'chai';
+
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+
+import * as fieldGenerator from './generate7xx';
+import {createValueInterface} from '../util';
+
+// Run tests
+generate776();
+
+// Test functions
+function generate776() {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', '..', '..', 'test-fixtures', 'transform', 'convert', 'common', 'generate7xx', 'generate776'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      reader: READERS.JSON,
+      failWhenNotFound: true
+    }
+  });
+
+  function callback({getFixture}) {
+    const input = getFixture('input.json');
+    const output = getFixture('output.json');
+
+    const valueInterface = createValueInterface(input);
+
+    const result = fieldGenerator.generate776(valueInterface);
+    expect(result).to.eql(output);
+  }
+}
