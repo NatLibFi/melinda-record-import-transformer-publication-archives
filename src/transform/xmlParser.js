61a1f0acb44b31f150f2f55eaaa67b28d05fcd18
aatuny <27770287+aatuny@users.noreply.github.com>
2024-09-12 11:02:51 +0300

    Change XML parser so that entitites such as &quot; and &amp; are parsed correctly

--- src/transform/xmlParser.js
+++ src/transform/xmlParser.js
@@ -1,18 +1,23 @@
 import {Parser} from 'xml2js';
 import {toXml} from 'xml-flow';
+import {DOMParser} from 'xmldom';
 
 /**
  * Convert XML to JS object
- * @param {string} node Read XML node
+ * @param {object} node Read XML node as object
  * @returns Parsed JS object
  */
 export function convertToObject(node) {
   const str = toXml(node);
-  return toObject();
+  return toObject(str.replaceAll('\\"', '&quot;')); // NB: escaped quote seems to be something xmldom cannot handle
 
-  function toObject() {
+  function toObject(str) {
     return new Promise((resolve, reject) => {
-      new Parser().parseString(str, (err, obj) => {
+      // NB: required for parsing entities such as &amp;
+      // see https://stackoverflow.com/a/41943379
+      const xmlString = new DOMParser().parseFromString(str, 'text/xml');
+
+      new Parser().parseString(xmlString, (err, obj) => {
         if (err) {
           /* istanbul ignore next: Generic error */ return reject(err);
         }
