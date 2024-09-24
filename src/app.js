33eb73786cbe6c7b39583fcab222107d9a8ecc4f
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-09-24 09:50:24 +0300

    update operator function name

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {transformerBlobLogic, createMongoOperator} from '@natlibfi/melinda-record-import-commons';
+import {transformerBlobLogic, createMongoBlobsOperator} from '@natlibfi/melinda-record-import-commons';
 import createTransformHandler from './transform';
 import amqplib from 'amqplib';
 
 export async function startApp(config) {
   const logger = createLogger();
 
-  const mongoOperator = await createMongoOperator(config.mongoUrl);
+  const mongoOperator = await createMongoBlobsOperator(config.mongoUrl);
   const transformHandler = createTransformHandler(config);
 
   logger.info('Starting melinda record import dc transformer');
