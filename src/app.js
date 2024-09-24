61812ba8fe0e23a91c992cb294a03409f4794a8c
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-09-24 09:24:51 +0300

    update deps and use mongo logic

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {transformerBlobLogic, createApiClient as createRecordImportApiClient, createMongoOperator} from '@natlibfi/melinda-record-import-commons';
+import {transformerBlobLogic, createMongoOperator} from '@natlibfi/melinda-record-import-commons';
 import createTransformHandler from './transform';
 import amqplib from 'amqplib';
 
 export async function startApp(config) {
   const logger = createLogger();
 
-  const mongoOperator = config.mongoUrl ? await createMongoOperator(config.mongoUrl) : false;
-  const riApiClient = await createRecordImportApiClient(config.recordImportApiOptions, config.keycloakOptions, mongoOperator);
+  const mongoOperator = await createMongoOperator(config.mongoUrl);
   const transformHandler = createTransformHandler(config);
 
   logger.info('Starting melinda record import dc transformer');
   try {
-    await transformerBlobLogic(riApiClient, transformHandler, amqplib, config);
+    await transformerBlobLogic(mongoOperator, transformHandler, amqplib, config);
   } catch (error) {
     logger.error(error);
   } finally {
